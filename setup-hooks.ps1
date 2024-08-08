b54663b96cb34da1df94004ccee374f8183bcc52
Ranjan Khatiwada <ranjan.kh11@gmail.com>
2024-08-07 21:19:18 -0400

    Cleaned and removed hooks files

--- setup-hooks.ps1
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env pwsh
-
-Copy-Item -Path .\hooks\post-checkout.ps1 -Destination .git\hooks\post-checkout.ps1
-Copy-Item -Path .\hooks\post-checkout.bat -Destination .git\hooks\post-checkout.bat
-
-Write-Host "Hooks installed successfully."
